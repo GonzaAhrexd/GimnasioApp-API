de217c774381750d2aaa1dfe3afe78ac0eeda1ab
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2026-01-26 21:50:59 -0300

    Agregados

--- src/controllers/professors.controller.ts
+++ src/controllers/professors.controller.ts
@@ -29,3 +29,60 @@ export const getProfessors = async (req: any, res: any) => {
         res.status(500).json({ message: 'Error fetching professors', error });
     }
 }
+
+export const getProfessorById = async (req: any, res: any) => {
+    const { id } = req.params;
+    try {
+        const professor = await prisma.profesor.findUnique({
+            where: { id_profesor: parseInt(id, 10) },
+        });
+        if (!professor) {
+            return res.status(404).json({ message: 'Professor not found' });
+        }
+        const user = await prisma.usuario.findUnique({
+            where: { id_usuario: professor.id_usuario },
+            select: {
+                id_usuario: true,
+                nombre: true,
+                email: true,
+                rol: true,
+                cambiarPass: true,
+            },
+        });
+        res.status(200).json({ ...professor, ...user });
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching professor', error });
+    }
+
+}
+
+export const searchProfesor = async (req: any, res: any) => {
+    const { email, nombre } = req.params;
+
+    try {
+
+        // Busca por nombre o email en la tabla de usuarios siempre que el rol sea profesor 
+   const users = await prisma.usuario.findMany({
+            where: {
+                ...(email && {
+                    email: {
+                        contains: email,
+                    },
+                    ...nombre && {
+                        nombre: {
+                            contains: nombre,
+                        }
+                    },
+                    rol: "profesor"                    
+                }),
+            }
+        });
+
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({ message: 'Error searching professors', error });
+    }
+
+
+}
